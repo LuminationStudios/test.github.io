6365b2c87c859a139331f17d47b07043db60c780
LuminationStudios <studios.lumination@gmail.com>
2025-10-08 00:31:18 -0600

    Update and rename bios.json to bios.js

---
+++ bios.js
@@ -0,0 +1,18 @@
+const bios = {
+  "": {
+    "name": "Welcome 🌸",
+    "bio": "This is a cool little bio site that is easy to use and setup. Use the button below to make your own bio!",
+  },
+  "artemis": {
+    "name": "Artemis",
+    "bio": "Pastel designer & coder 💕",
+    "image": "images/placeholder.png",
+    "links": { "Instagram": "https://instagram.com/artiedesigns" }
+  },
+  "test": {
+    "name": "test",
+    "bio": "PTest",
+    "image": "images/placeholder.png",
+    "links": { "Instagram": "https://instagram.com/" }
+  }
+};

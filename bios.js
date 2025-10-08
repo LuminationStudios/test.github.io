cda69a60a7c87660c83eefb3af23945544dde92e
LuminationStudios <studios.lumination@gmail.com>
2025-10-08 00:53:40 -0600

    Update bios.js

--- bios.js
+++ bios.js
@@ -1,12 +1,10 @@
 window.bios = {
-  "": {
-    "name": "Welcome 🌸",
-    "bio": "This is your bio site. Use the button to create your own card.",
-  },
   "artemis": {
     "name": "Artemis",
     "bio": "Pastel designer & coder 💕",
     "image": "images/placeholder.png",
-    "links": { "Instagram": "https://instagram.com/artiedesigns" }
+    "links": {
+      "Instagram": "https://instagram.com/artiedesigns"
+    }
   }
 };
